d773611f30850d9757c36edeabd0b4327d46f35e
LeeJungHoon <gns14585@naver.com>
2023-09-18 17:47:53 +0900

    Practice

---
+++ Practice/p31_p60/HashSetExample.java
@@ -0,0 +1,32 @@
+package Practice.p31_p60;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+
+        set.add("Java");
+        set.add("JDBC");
+        set.add("JSP");
+        set.add("Spring");
+
+        Iterator<String> iterator = set.iterator();
+        while (iterator.hasNext()) {
+            String n = iterator.next();
+            System.out.println(n);
+            if (n.equals("JSP")) {
+                iterator.remove();
+            }
+        }
+        System.out.println();
+
+        set.remove("Spring");
+
+        for (String m : set) {
+            System.out.println(m);
+        }
+    }
+}
---
+++ Practice/p31_p60/p37/ArrayListExample.java
@@ -0,0 +1,30 @@
+package Practice.p31_p60.p37;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List<Board> list = new ArrayList<>();
+
+        list.add(new Board("제목1", "내용1", "글쓴이1"));
+        list.add(new Board("제목2", "내용2", "글쓴이2"));
+        list.add(new Board("제목3", "내용3", "글쓴이3"));
+        list.add(new Board("제목4", "내용4", "글쓴이4"));
+        list.add(new Board("제목5", "내용5", "글쓴이5"));
+
+        int size = list.size();
+        System.out.println("총 객체 수 : " + size);
+        System.out.println();
+
+        // 특정 인덱스 객체 가져오기
+        Board board = list.get(2);
+        System.out.println(board.getContent() + "\t" + board.getSubject() + "\t" + board.getWriter());
+        System.out.println();
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(board.getContent() + "\t" + board.getSubject() + "\t" + board.getWriter());
+        }
+
+    }
+}
